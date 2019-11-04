99dc3b00314de26bca0f16aae127c0fb34775a58
dianedesira <33417714+dianedesira@users.noreply.github.com>
2019-11-04 09:20:37 +0100

    Transferred to new Repository

---
+++ BlockBreakerSWD42D/Assets/Scripts/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle mypaddle;
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yVelocity = 15f;
+
+    [SerializeField] AudioClip[] ballsounds; 
+
+
+    Vector2 paddleToBallDistance;
+
+    bool hasStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = this.transform.position - mypaddle.transform.position;
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (!hasStarted)
+        {
+            LockBalltoPaddle();
+            LaunchOnMouseClick();
+        }
+
+
+    }
+
+
+    void LockBalltoPaddle()
+    {
+        Vector2 mypaddleposition = new Vector2(mypaddle.transform.position.x, mypaddle.transform.position.y);
+
+        this.transform.position = mypaddleposition + paddleToBallDistance;
+
+    }
+
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity + Random.Range(0.0f, 2f) , yVelocity + Random.Range(0.0f,2f));
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //  if other.gameObject==
+        if (other.gameObject.tag == "Wall")
+        {
+            AudioClip clip = ballsounds[0];
+            this.GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+
+        if (other.gameObject.name == "Paddle")
+        {
+            AudioClip clip = ballsounds[1];
+            this.GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+
+
+
+    }
+
+
+}
---
+++ BlockBreakerSWD42D/Assets/Scripts/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float ScreenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float mousePosition = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        Debug.Log(mousePosition);
+        float limitMousePosition = Mathf.Clamp(mousePosition, minX, maxX);
+        Debug.Log(limitMousePosition);
+        //print(Input.mousePosition.x);
+        Vector2 paddlePosition = new Vector2(limitMousePosition, transform.transform.position.y);
+         transform.position = paddlePosition;
+        
+
+        
+        //teacher`s solution
+        //float xPoint = Input.mousePosition.x;
+        //transform.position = Camera.main.ScreenToViewportPoint(new Vector3(xPoint,0,0)) -new Vector3 (0,-1,Camera.main.transform.position.z);
+
+    }
+
+ //   void OnCollisionEnter2D(Collision2D other)
+ //   {
+      //  if other.gameObject==
+ //       this.GetComponent<AudioSource>().Play();
+
+//    }
+}
