adecbaa4948545e966b37f237c040b48ee9867cb
dianedesira <33417714+dianedesira@users.noreply.github.com>
2019-11-04 16:07:33 +0100

    Comments + PlayClipAtPoint

--- BlockBreakerSWD42D/Assets/Scripts/Block.cs
+++ BlockBreakerSWD42D/Assets/Scripts/Block.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
     private void OnCollisionEnter2D(Collision2D coll)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        /* Using PlayOneShot would not play the break sound since the block would
+         * have been destroyed and so would the AudioSource.
+         * PlayClipAtPoint sets the AudioClip in a position and plays it there.
+         * */
         Destroy(this.gameObject);
     }
     
