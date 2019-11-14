9f076f7e388cc462a56d84f44f0238a8e20795e9
dianedesira <33417714+dianedesira@users.noreply.github.com>
2019-11-14 13:24:06 +0100

    Displayed Score

--- BlockBreakerSWD42D/Assets/Scripts/Block.cs
+++ BlockBreakerSWD42D/Assets/Scripts/Block.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
-    Level level; // level is a copy/instance of the Level class (we need to use it to access its elements
+    Level level; // level is a copy/instance of the Level class 
+    //(we need to use it to access its elements
     // e.g. variables and methods)
+
+    GameStatus gameStatus;
 
     void Start()
     {
+        gameStatus = FindObjectOfType<GameStatus>();
+
         level = FindObjectOfType<Level>();
         /* The FindObjectOfType method looks for an object in the hierarchy containing the script 
          * having the same name as the class written in between <>
@@ -19,6 +24,7 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
+        gameStatus.AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         /* Using PlayOneShot would not play the break sound since the block would
          * have been destroyed and so would the AudioSource.
--- BlockBreakerSWD42D/Assets/Scripts/GameStatus.cs
+++ BlockBreakerSWD42D/Assets/Scripts/GameStatus.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
     [SerializeField] int currentScore = 0;
     [SerializeField] int pointsPerBlockDestroyed = 10;
+
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        scoreText = FindObjectOfType<TextMeshProUGUI>();
+        scoreText.text = currentScore.ToString();
+    }
+    
+    //This method needs to be called whenever a block is destroyed
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        //currentScore = currentScore + pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
 }
