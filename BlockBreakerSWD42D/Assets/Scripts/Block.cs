508351f03ded815f3881b8dbe236f5c1ef44e3fc
dianedesira <33417714+dianedesira@users.noreply.github.com>
2019-11-25 16:18:19 +0100

    Particle Effect, tags + resources

--- BlockBreakerSWD42D/Assets/Scripts/Block.cs
+++ BlockBreakerSWD42D/Assets/Scripts/Block.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX; //type used for prefabs is GameObject
+
     Level level; // level is a copy/instance of the Level class 
     //(we need to use it to access its elements
     // e.g. variables and methods)
@@ -19,7 +21,9 @@ public class Block : MonoBehaviour
         /* The FindObjectOfType method looks for an object in the hierarchy containing the script 
          * having the same name as the class written in between <>
          */
-        level.CountBreakableBlocks();
+
+        if(tag == "breakable")
+            level.CountBreakableBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D coll)
@@ -30,8 +34,30 @@ public class Block : MonoBehaviour
          * have been destroyed and so would the AudioSource.
          * PlayClipAtPoint sets the AudioClip in a position and plays it there.
          * */
-        level.BlockDestroyed();
-        Destroy(this.gameObject);
+
+        if (tag == "breakable")
+        {
+            TriggerSparklesVFX();
+            level.BlockDestroyed();
+            Destroy(this.gameObject);
+            /* The gameObject keyword with a g lowercase is referring to the object in which this
+             * current script is attached [in this case we are referring to the current Block]
+             * The GameObject keyword with a G uppercase refers to the GameObject class/type
+             * storing all general data and methods related to Unity game objects
+             */
+        }
+    }
+
+    void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        /* The Instantiate method creates a copy of the object passed as the first arguement/parameter
+         * [in our case blockSparklesVFX] - it replicates the duplicate task we do in the editor
+         * We need to also indicate the position so that the copy is placed in the same position as
+         * the block which is currently being destroyed.
+         */
+
+        Destroy(sparkles, 1f);
     }
     
 }
---
+++ BlockBreakerSWD42D/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    GameObject[] blocks;
+    GameObject randomBlock;
+
+    Vector2 blockPosition, startingPosition; //Vector2 since we need to store the x and y coordinates
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
